29a5c7af7d2f91d8cc000304a5cf87d88c6dbc4a
Martina Amore <martina.amore02@gmail.com>
2021-01-02 12:26:50 +0100

    Snack 6 completo

---
+++ snack-6/index.php
@@ -0,0 +1,48 @@
+<!-- SNACK 6: Utilizzare questo array: https://pastebin.com/CkX3680A.
+Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio
+e i PM in un rettangolo verde. -->
+
+<?php
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+?>
+
+<?php
+
+    foreach ($db as $key => $value) {
+        echo $div = "<div style='background-color: lightgrey;'>";
+        if ($key == 'pm') {
+            echo str_replace("lightgrey;", "lightgreen;", $div);
+        }
+        echo ucfirst($key) ."<br><br>";
+        foreach ($value as $element) {
+            echo "Name: " .$element["name"] ."\n" ."<br>"
+            ."Lastname: " .$element["lastname"]
+            ."<br><br>";
+        }
+        echo "</div>";
+    }
+
+?>
